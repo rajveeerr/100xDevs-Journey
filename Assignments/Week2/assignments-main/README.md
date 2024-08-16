69d5777d0619363195a73cb08df1c9451db1ca4a
Rajveer Singh <rajveergreets@gmail.com>
2024-08-16 15:27:01 +0530

    Delete Assignments/Week2 directory

--- Assignments/Week2/assignments-main/README.md
+++
@@ -1,1 +0,0 @@
-100xdevs Cohort 3 Assignments
--- Assignments/Week2/assignments-main/week-2/week-2-js/hard/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-tion timer(s){
-//     let counter=1;
-// }
-// let counter=1;
-// while(counter<=10){
-//     setTimeout(()=>console.log(counter),1000);
-//     counter++;
-// }
