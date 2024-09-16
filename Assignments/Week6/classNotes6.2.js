60916e1f1a52c4becd9b89c20088f2ee34490818
Rajveer Singh <rajveergreets@gmail.com>
2024-09-17 00:17:29 +0530

    Add files via upload

--- Assignments/Week6/classNotes6.2.js
+++ Assignments/Week6/classNotes6.2.js
@@ -20,19 +20,23 @@ let users=[]
 function auth(req,res,next){//this is very flawed logic, it works well if the requests are just made by fe, but if someone request this using postman and sends a token thata contains a username that doesn't exist on the server/db this auth will still show them they are logged and give acess to the endpoint, where the endpoint will try to extract the data for the non-existant user and will throw err, i can handle this error there to say ki user doesnt exist but this is wrong, to make this correct i can call database and chack if user exists in db, but this defets the whole purpose of jwt, well actually not i can simpley call the database requesting the data of user here and if the data exists i will modify the req object to contain this data and pass to the next fn so they dont need to call the database again, if the data is empty this simply means user doesn't exists
     if(req.headers.authorization){
         let reqToken=req.headers.authorization;
-        let decodedData=jwt.verify(reqToken,JWT_SECRET);
-        if(decodedData.username){
-            req.username=decodedData.username;
-            // res.json({message: "Authorization succesfull!!! Redirecting..."})//can send data in body back only once so dont do it here
-            next();//since the token exists that means user has logged in and hen can acess endpoint, we cant pass parameter to next, as it will not reach the next fn 
-        }
-        else{
-            res.status(401).json({
-                message: "Invalid token provided!!!!"
-            })
-
-        }
-        
+        jwt.verify(reqToken,JWT_SECRET,(err,decoded)=>{
+            if(err){
+                res.status(401).json({
+                    message: "Invalid token provided!!!!"
+                })
+            }
+            else if(decoded.username){
+                req.username=decoded.username;
+                // res.json({message: "Authorization succesfull!!! Redirecting..."})//can send data in body back only once so dont do it here
+                next();//since the token exists that means user has logged in and hen can acess endpoint, we cant pass parameter to next, as it will not reach the next fn 
+            }
+            else{
+                res.status(401).json({
+                    message: "Invalid token provided!!!!"
+                })
+            }
+        });
     }
     else{
         res.status(401).json({
@@ -43,6 +47,8 @@ function auth(req,res,next){//this is very flawed logic, it works well if the re
 
 app.use(express.json());
 app.use(cors());
+
+// app.use(express.static("./public")) //this middleware will serve all the static files in public folder and files can be acessed by putting their name in url
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/public/index.html");//this wll host fe and be on same server no need of cors
