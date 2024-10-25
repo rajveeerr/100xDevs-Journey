5109fa0f6e96cce5d0f7f0c8798f1667917f7029
Rajveer Singh <rajveergreets@gmail.com>
2024-10-25 18:10:41 +0530

    removed redundant directory

--- Assignments/Week9/recordedClassNotes9.3/notes.js
+++
@@ -1,4 +0,0 @@
-// we use vite to initialise and run react projects by converting them to html css and js, it lets us reun both dev and 
-// production server locally
-
-//in pkg file, for web apps we use react-dom as dependency and for apps we use react-native as dependency
