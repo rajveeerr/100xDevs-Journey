08ed7debae72bd0973bb1737404301173c977824
Rajveer Singh <rajveergreets@gmail.com>
2024-11-24 05:31:42 +0530

    Add files via upload

--- Assignments/Week11/recoil-offline/src/components/chatBot.jsx
+++ Assignments/Week11/recoil-offline/src/components/chatBot.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useRef, useState } from 'react'
-import { atom, useRecoilState, useRecoilValue, useRecoilStateLoadable, atomFamily, selectorFamily } from 'recoil'
+import { atom, useRecoilState, useSetRecoilState ,useRecoilValue, useRecoilStateLoadable, atomFamily, selectorFamily } from 'recoil'
 import '../ChatComponent.css'
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import 'highlight.js/styles/github-dark.css';
+import remarkGfm from "remark-gfm";
+import { v4 as uuidv4 } from 'uuid';
+import { Rnd } from 'react-rnd';
 
 let geminiApi=import.meta.env.VITE_GEMINI_API;
-  
+let botIcon='./logoImg.jpg'
 
 export default function ChatSection(){
-
-  return <div className='chat-section'>
+{/*<Rnd>*/}
+  return <Rnd style={{zIndex:2}}><div className='chat-section'>
     <Heading/>
     <ChatArea/>
     <Input/>
   </div>
+  </Rnd>
 }
 
 function Heading(){
@@ -34,6 +38,7 @@ function handleCopy(){
 function ChatArea(){
   let [chatHistory,setChatHistory]=useRecoilState(allChats)//[{question: id,answer: id},]
   let chatArea=useRef();
+  console.log(chatHistory);
   
   useEffect(()=>{
     scrollToBottom(chatArea.current)
@@ -42,22 +47,28 @@ function ChatArea(){
   return <div className='chat-area' ref={chatArea}>
     <Online/>
     <HeroIntro/>
+    {/* {console.log(chatHistory)} */}
+    
     {chatHistory.map((history,index)=>{
-      return(<><Question key={index} id={history.question}/><Chat id={history.answer} key={index} questionId={history.question}/></>)
+      return(<><Question key={index+history.question} id={history.question}/><Chat key={index} questionId={history.question}/></>)
     })}
   </div>
 }
 
 function Question(props){
-  let botIcon='./logoImg.jpg'
+  
   let [currentQuestion,setQuestion]=useRecoilState(questionFamily(props.id))
+  // console.log(currentQuestion);
+  
   let [editing,setEditing]=useState(false)
   let questionArea=useRef()
+  let originalQuestion=useRef()
 
   function handleEdit(){
     questionArea.current.contentEditable="true"
     questionArea.current.classList.add("editing")
     setEditing(editing=>!editing)
+    originalQuestion.current=questionArea.current.innerText
   }
   function handleSubmit(){
     questionArea.current.contentEditable="false"
@@ -70,26 +81,44 @@ function Question(props){
     questionArea.current.contentEditable="false"
     questionArea.current.classList.remove("editing")
     setEditing(editing=>!editing)
-    // setQuestion(q=>({...q,question:questionArea.current.value}))
+
+    questionArea.current.innerText=originalQuestion.current
   }
 
-  return <div className='question-container'><div className='question'>
-    <span className='bot-icon'><img src={botIcon}></img></span>
-    <p className='question-txt' ref={questionArea}>{currentQuestion.question}</p>
-    {!editing&&<span className='edit-icon' onClick={handleEdit}><i class="fa-solid fa-pen"></i></span>}
+  return <div className='question-container'>
+    <div className='question'>
+      <span className='bot-icon'><img src={botIcon}></img></span>
+      <p className='question-txt' ref={questionArea}>{currentQuestion.question}</p>
+      {!editing&&<span className='edit-icon' onClick={handleEdit}><i class="fa-solid fa-pen"></i></span>}
     </div>
     {editing&&<div className='chat-options edit-options'>
         <span onClick={handleSubmit}  className='copy chat-option'><span className='copy-icon'><i class="fa-regular fa-clipboard"></i></span>Save</span>
         <span onClick={handleCancel} className='collection chat-option'><span className='collection-icon'><i class="fa-solid fa-xmark"></i></span>Cancel</span>
-      </div>}
-      </div>
+    </div>}
+  </div>
 }
 
 function Chat(props){
   let askedQuestion=useRecoilValue(questionFamily(props.questionId))
-  let [currentAnswer,setAnswer]=useRecoilStateLoadable(answerFamily({id:props.id,question:askedQuestion.question}))
-  //source of this id is input component, its first passing it to chatarea component and then here
+  let answerId=useRef(uuidv4())//this id will be generated for every new chat when it will mount
+  let [currentAnswer,setAnswer]=useRecoilStateLoadable(answerFamily({id:answerId.current,question:askedQuestion.question}))
+  let [chatHistory,setChatHistory]=useRecoilState(allChats)
+
+  useEffect(()=>{
+    let updatedAnswerId=chatHistory.map(chat=> 
+      chat.question===props.questionId?{ ...chat, answer: answerId}:chat
+    );
+    setChatHistory(updatedAnswerId)
+  },[])
+  // workflow=> question typed and enter is pressed from input element, this will generate an atom with an unique id which will update
+  // everytime a question is entered, and the chat history is being updated with the question id, now chat area renders the chat 
+  // history(which contains the question id and answer id) and renders the chat component and question component for those ids, now
+  // question component simply fetch the value of atom from respective atomFamily using those ids to render them, chat coponent
+  // generated a new answer id, for the question id and question received, and updated the chatHistory with the answer id mapped to
+  // the respective question id, this change of chathistory array triggers the re-render of all the answer and chat components onscreen. 
   
+  // edit-workflow=>
+
   if(currentAnswer.state==='loading'){
 
     return <div className='chat'>
@@ -115,7 +144,7 @@ function Chat(props){
   return <div className='chat'>
     <div className='user-icon'><img src={currentAnswer.contents.userIcon}></img></div>
     <div className='chat-and-options'>
-    <p className='chat-content'><ReactMarkdown rehypePlugins={[rehypeHighlight]}>{currentAnswer.contents.answer}</ReactMarkdown></p>
+    <p className='chat-content'><ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>{currentAnswer.contents.answer}</ReactMarkdown></p>
   
       {/* <p className='chat-content'>{currentAnswer.contents.answer}</p> */}
       <div className='chat-options'>
@@ -128,11 +157,17 @@ function Chat(props){
 function HeroIntro(){
   return<div className='hero-section'>
     {/* <h1 className='hero-sup-title'>SUPERPOWERED</h1> */}
-    <h1 className='hero-title'>1OxAnswers</h1>
+    <div className='floating-icons'>
+      <span className='icon forward forward-first'><i class="fa-solid fa-robot"></i></span>
+      <span className='icon forward forward-second'><i class="fa-solid fa-comments-dollar"></i></span>
+      <span className='icon backward backward-first'><i class="fa-regular fa-compass"></i></span>
+      <span className='icon backward backward-second'><i class="fa-solid fa-sitemap"></i></span>
+    </div>
+    <h1 className='hero-title'><span className='stylize'>10x</span>Answers</h1>
     <h2 className='hero-sub-title'>Because your Questions should not be left un-answered.</h2>
     <span style={{background:"rgb(44 44 44)",padding:"0.3rem 1.1rem",borderRadius:"1.6rem",margin:"1rem"}}>
       <p className='hero-description'>Start Asking you Burning Questions</p>
-      </span>
+    </span>
   </div>
 }
 
@@ -147,7 +182,6 @@ let answerFamily=atomFamily({
     key: "fetchAnswers",
     get: ({id,question})=>{
       historyPrompts.push(question)
-      console.log(historyPrompts);
       
       return async()=>{
         let url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key="+geminiApi;
@@ -192,34 +226,38 @@ let allChats=atom({
 function Input(){
   let inputElement=useRef()
   let submitBtn=useRef()
-  let id=useRef(1);
-  let [currentQuestion,setQuestion]=useRecoilState(questionFamily(id.current))
-  let [chatHistory,setChatHistory]=useRecoilState(allChats);
-
-  const handleKeyDown = (event) => {
+  // let id=useRef(1);
+  let id=useRef(uuidv4());//this will keep changing when compounts mounts
+  let setQuestion=useSetRecoilState(questionFamily(id.current))
+  let [history,setChatHistory]=useRecoilState(allChats);
+  //figure out why this code is breaking whe useSetRecoilState is used
+  
+  function handleKeyDown(event){
+    inputElement.current.value.trim().length!=0?submitBtn.current.classList.remove("disabled"):submitBtn.current.classList.add("disabled")
     if (event.key === 'Enter') {
       event.preventDefault()
-      submitBtn.current.classList.add("disabled")
-      if(inputElement.current.value!=""){
+      if(inputElement.current.value.trim().length!=0){
         setQuestion(q=>({...q,question: inputElement.current.value}))
-        setChatHistory(history=>[...history,{question:id.current,answer:id.current}])
-        id.current+=1;
+        setChatHistory(history=>[...history,{question:id.current,answer:null}])
+        id.current=uuidv4();//changing the id
         inputElement.current.value="";
+        submitBtn.current.classList.add("disabled")
       }
     }
   } 
   function handleSubmit(){
     if(inputElement.current.value!=""){
       setQuestion(q=>({...q,question: inputElement.current.value}))
-      setChatHistory(history=>[...history,{question:id.current,answer:id.current}])
-      id.current+=1;
+      setChatHistory(history=>[...history,{question:id.current,answer:null}])
+      id.current=uuidv4;
       inputElement.current.value="";
       submitBtn.current.classList.add("disabled")
     }
-}
+  }
 
+  
   return <div className='input-area'>
-    <textarea ref={inputElement} onKeyUp={(e)=>{(e.target.value!="")?submitBtn.current.classList.remove("disabled"):null}} onKeyDown={handleKeyDown} placeholder='Ask or search anything and press Enter' className='prompt-input'></textarea>
+    <textarea ref={inputElement} onKeyUp={handleKeyDown} placeholder='Ask or search anything and press Enter' className='prompt-input'></textarea>
     <span ref={submitBtn} onClick={handleSubmit} className='search-icon disabled' title="Send"><i class="fa-solid fa-square-arrow-up-right"></i></span>
     <InputOptions/>
   </div>
@@ -267,11 +305,9 @@ function Online(){
   </div>
 }
 // last streach: 
-// HIGH PRIORITY=> fix bugs: prolly about the ids assigning
+// HIGH PRIORITY=> fix bugs: prolly about the ids assigning - done
 // make it persistant
 // npm-package out of ot
 // THE MAIN THING: Figure out why would anyone use it??? S omething sarchamesic?? Some responses that are not provided by anyone
 // figure out why extra re-rendering
 // cleanify the code 
-// if possible try removing all chats after the edited one
-// can shift to groq
